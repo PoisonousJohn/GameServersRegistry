c18442a1b35825b8c7f704249a1b7f4e3712749d
Ivan Fateev <ivan.fateev@microsoft.com>
2017-08-24 12:58:13 +0300

    initial code

---
+++ GameServerRegistry/Controllers/ServersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace GameServerRegistry.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServersController : Controller
+    {
+
+        public ServersController(IServersRegistry serversRegistry, ILogger<ServersController> logger)
+        {
+            _serversRegistry = serversRegistry;
+            _log = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _serversRegistry.GetServers().Select(i => i.ToString());
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult Post([FromBody]int port)
+        {
+            var ip = Request.HttpContext.Connection.RemoteIpAddress;
+            var endpoint = new System.Net.IPEndPoint(ip, port);
+            _serversRegistry.RegisterServer(endpoint);
+            _log.LogWarning($"Got ping from server {endpoint}");
+            return Ok();
+        }
+
+        private static readonly Dictionary<System.Net.EndPoint, DateTime> registry
+                                                    = new Dictionary<System.Net.EndPoint, DateTime>();
+        private readonly IServersRegistry _serversRegistry;
+        private readonly ILogger<ServersController> _log;
+    }
+
+}
---
+++ GameServerRegistry/ServersRegistry.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace GameServerRegistry
+{
+    public interface IServersRegistry
+    {
+        IEnumerable<IPEndPoint> GetServers();
+        void RegisterServer(IPEndPoint server);
+    }
+    public class ServersRegistry : IServersRegistry
+    {
+        public ServersRegistry(ILogger<ServersRegistry> log)
+        {
+            _checkServers = new Timer(RemoveDeadServers, null, 0, 1000);
+            _log = log;
+        }
+
+        public IEnumerable<IPEndPoint> GetServers()
+        {
+            return _servers.Keys;
+        }
+
+        public void RegisterServer(IPEndPoint server)
+        {
+            lock (_servers)
+            {
+                _servers[server] = DateTime.Now;
+            }
+        }
+
+        private void RemoveDeadServers(object state)
+        {
+            lock (_servers)
+            {
+                var now = DateTime.Now;
+                var serversToRemove = new List<IPEndPoint>();
+                foreach (var server in _servers)
+                {
+                    if ((now - server.Value).TotalSeconds >= SERVER_TIMEOUT_SECONDS)
+                    {
+                        serversToRemove.Add(server.Key);
+                    }
+                }
+
+                foreach (var server in serversToRemove)
+                {
+                    _log.LogCritical($"Server {server} timed out. Removing");
+                    _servers.Remove(server);
+                }
+            }
+        }
+
+        private Dictionary<IPEndPoint, DateTime> _servers = new Dictionary<IPEndPoint, DateTime>();
+        private Timer _checkServers;
+        private const int SERVER_TIMEOUT_SECONDS = 10;
+        private readonly ILogger<ServersRegistry> _log;
+    }
+}
